fbe07dd41f786649f0f643be43a58404c3d92d57
bharath <bharathbk2121@gmail.com>
2024-06-20 17:55:21 +0530

    Added Rating model

---
+++ src/main/java/com/application/cab_application/Models/Rating.java
@@ -0,0 +1,40 @@
+package com.application.cab_application.Models;
+
+public class Rating {
+    private int id;
+    private String comments ;
+    private double ratingValue ;
+    private int rideID ;
+
+    public Rating() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public double getRatingValue() {
+        return ratingValue;
+    }
+
+    public int getRideID() {
+        return rideID;
+    }
+
+    public Rating(int rideID, double ratingValue, String comments) {
+        this.rideID = rideID;
+        this.ratingValue = ratingValue;
+        this.comments = comments;
+    }
+
+    public Rating(int id, String comments, double ratingValue, int rideID) {
+        this.id = id;
+        this.comments = comments;
+        this.ratingValue = ratingValue;
+        this.rideID = rideID;
+    }
+}
