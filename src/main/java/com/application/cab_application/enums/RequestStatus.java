55160947601345402099779298613c61565e2373
bharath <bharathbk2121@gmail.com>
2024-06-20 14:57:58 +0530

    Added ENUM types to store the enums are numbers in database and validate those enums while retrieving

--- src/main/java/com/application/cab_application/enums/RequestStatus.java
+++ src/main/java/com/application/cab_application/enums/RequestStatus.java
@@ -1,9 +1,28 @@
 package com.application.cab_application.enums;
 
 public enum RequestStatus {
-    WAITING,
-    ACCEPTED,
-    STARTED,
-    ENDED,
-    CANCELLED
+    WAITING(1),
+    ACCEPTED(2),
+    STARTED(3),
+    ENDED(4),
+    CANCELLED(5);
+
+    private final int code ;
+
+    RequestStatus(int code){
+        this.code = code;
+    }
+
+    public int getCode(){
+        return code ;
+    }
+
+    public RequestStatus fromCode(int code){
+        for(RequestStatus requestStatus: RequestStatus.values()){
+            if(requestStatus.getCode() == code)
+                return requestStatus ;
+        }
+        throw new IllegalArgumentException("Unknown code " +code);
+    }
+
 }
