9e188eb54fadec3cc97efcbc7c4b5709949e500e
bharath <bharathbk2121@gmail.com>
2024-06-20 18:42:09 +0530

    Ride Details DAO layer

--- src/main/java/com/application/cab_application/DAO/RideDetailsDao.java
+++ src/main/java/com/application/cab_application/DAO/RideDetailsDao.java
@@ -8,10 +8,15 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class RideDetailsDao {
-    public int createRiderDetail(RideDetails rideDetails) {
+    public static int createRiderDetail(RideDetails rideDetails) {
         String query = "insert into ride_details(from_location_id, to_location_id, ride_status, start_time, end_time) values (?,?,?,?,?)";
         ResultSet resultSet;
         try (PreparedStatement preparedStatement = DatabaseConnector.getConnection().prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setInt(1, rideDetails.getFromLocation());
+            preparedStatement.setInt(2, rideDetails.getToLocation());
+            preparedStatement.setInt(3, rideDetails.getRequestStatus().getCode());
+            preparedStatement.setTimestamp(5, rideDetails.getStartTime());
+            preparedStatement.setTimestamp(6, rideDetails.getEndTime());
             int affectedRows = preparedStatement.executeUpdate();
             if (affectedRows > 0) {
                 resultSet = preparedStatement.getGeneratedKeys();
@@ -30,14 +35,19 @@ public class RideDetailsDao {
         ResultSet resultSet;
         try (PreparedStatement preparedStatement = DatabaseConnector.getConnection().prepareStatement(query)) {
             resultSet = preparedStatement.executeQuery();
-            if(resultSet.isBeforeFirst()){
+            if (resultSet.isBeforeFirst()) {
                 resultSet.next();
-                return new RideDetails(resultSet.getInt("id"),resultSet.getInt("from_location_id"),
-                        resultSet.getInt("to_location_id"), RequestStatus.fromCode(resultSet.getInt("request_status")));
+                return new RideDetails(resultSet.getInt("id"), resultSet.getInt("from_location_id"),
+                        resultSet.getInt("to_location_id"), RequestStatus.fromCode(resultSet.getInt("request_status")),
+                        resultSet.getTimestamp("start_time"), resultSet.getTimestamp("end_time"));
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         return new RideDetails();
+    }
+
+    public static boolean updateRideDetails(RideDetails rideDetails) {
+
     }
 }
