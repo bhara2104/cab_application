8af3540ebf36afea6a72d55c0717b777a31776cc
bharath <bharathbk2121@gmail.com>
2024-06-20 12:23:15 +0530

    Added an Enum Function to convert enum code into account type and account type into enum code

--- src/main/java/com/application/cab_application/enums/AccountType.java
+++ src/main/java/com/application/cab_application/enums/AccountType.java
@@ -1,6 +1,24 @@
 package com.application.cab_application.enums;
 
 public enum AccountType {
-    RIDER,
-    DRIVER
+    RIDER(1),
+    DRIVER(2);
+
+    private final int code ;
+    AccountType(int code){
+        this.code = code;
+    }
+
+    public int getCode(){
+        return code;
+    }
+
+    public static AccountType fromCode(int code) {
+        for (AccountType status : AccountType.values()) {
+            if (status.getCode() == code) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Unknown code: " + code);
+    }
 }
