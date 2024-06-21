f75cfadd7a5573272cc070b3dd91f9b2831fa77f
bharath <bharathbk2121@gmail.com>
2024-06-21 14:56:22 +0530

    Ride Details

--- src/main/java/com/application/cab_application/Services/RideServices.java
+++ src/main/java/com/application/cab_application/Services/RideServices.java
@@ -1,5 +1,6 @@
 package com.application.cab_application.Services;
 
+import com.application.cab_application.DAO.RideDetailsDao;
 import com.application.cab_application.DAO.RidesDao;
 import com.application.cab_application.Models.Ride;
 import com.application.cab_application.Models.RideDetails;
@@ -10,22 +11,30 @@ import com.google.gson.JsonObject;
 import java.sql.Connection;
 
 public class RideServices {
-    public int createRide(String jsonBody){
+    public static boolean createRide(String jsonBody) {
         Gson gson = new Gson();
         JsonObject jsonObject = gson.fromJson(jsonBody, JsonObject.class);
         JsonObject ride = jsonObject.getAsJsonObject("ride");
         JsonObject rideDetails = jsonObject.getAsJsonObject("ride_details");
         Ride rideObject = gson.fromJson(ride, Ride.class);
         RideDetails rideDetails1 = gson.fromJson(rideDetails, RideDetails.class);
-        try(Connection connection = DatabaseConnector.getConnection()){
+        try (Connection connection = DatabaseConnector.getConnection()) {
             connection.setAutoCommit(false);
-            int id = RidesDao.createRide(rideObject);
-            if(id !=0){
+            try {
+                int id = RidesDao.createRide(rideObject);
+                if (id == 0) {
+                    return false;
+                }
                 rideDetails1.setRideID(id);
+                RideDetailsDao.createRiderDetail(rideDetails1);
+                connection.commit();
+                return true;
+            } catch (Exception e) {
+                connection.rollback();
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        return 0;
+        return false;
     }
 }
--- src/main/java/com/application/cab_application/Servlets/RideServlet.java
+++ src/main/java/com/application/cab_application/Servlets/RideServlet.java
@@ -2,6 +2,7 @@ package com.application.cab_application.Servlets;
 
 import java.io.*;
 
+import com.application.cab_application.Services.RideServices;
 import com.application.cab_application.Util.ReadJson;
 import jakarta.servlet.http.*;
 
@@ -22,6 +23,15 @@ public class RideServlet extends HttpServlet {
         String requestBody = ReadJson.convertJsonToString(request.getReader());
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
+        PrintWriter printWriter = response.getWriter();
+        boolean result = RideServices.createRide(requestBody);
+        if (result) {
+            response.setStatus(HttpServletResponse.SC_CREATED);
+            printWriter.write("{\"message\":\"Ride created successfully\"}");
+        } else {
+            response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+            printWriter.write("{\"message\":\"Ride creation not successful\"}");
+        }
     }
 
     public void destroy() {
