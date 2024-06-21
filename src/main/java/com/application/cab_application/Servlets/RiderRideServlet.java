30edd331d6265d1d417ab337bf654b423ecd7efe
bharath <bharathbk2121@gmail.com>
2024-06-21 15:49:56 +0530

    Added Servlet mapping rider driver and driver rides

---
+++ src/main/java/com/application/cab_application/Servlets/RiderRideServlet.java
@@ -0,0 +1,24 @@
+package com.application.cab_application.Servlets;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "RiderRideServlet", value = "/RiderRideServlet")
+public class RiderRideServlet extends HttpServlet {
+
+    public void init() {
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+    public void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+    public void destroy() {
+    }
+}
