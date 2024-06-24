94e32f522673b9d69e87196ec5a4a7e8a69585e0
bharath <bharathbk2121@gmail.com>
2024-06-24 14:39:12 +0530

    Saved upi servlet to fetch UPI ID's

---
+++ src/main/java/com/application/cab_application/Servlets/SavedUPIServlet.java
@@ -0,0 +1,28 @@
+package com.application.cab_application.Servlets;
+
+import java.io.*;
+import java.util.List;
+
+import com.application.cab_application.DAO.UpiDataDao;
+import com.application.cab_application.Models.UpiData;
+import com.google.gson.Gson;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "SavedUPIServlet", value = "/SavedUPIServlet")
+public class SavedUPIServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String accountID = request.getParameter("accountID");
+        int accountId = Integer.parseInt(accountID);
+        List<UpiData> upiDataList = UpiDataDao.getSavedUPI(accountId);
+        PrintWriter printWriter = response.getWriter();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(200);
+        if(upiDataList.isEmpty()){
+            printWriter.write("{\"message\":\"No Saved UPI Exist\"}");
+        }else{
+            printWriter.write(new Gson().toJson(upiDataList));
+        }
+    }
+}
