6825728d6281d007ce637dcd51dac2d51fce9df2
bharath <bharathbk2121@gmail.com>
2024-06-25 11:45:25 +0530

    Added Routes for driver login and rider login and added bcrypt dependency and json dependency

---
+++ src/main/java/com/application/cab_application/Servlets/DriverLoginServlet.java
@@ -0,0 +1,24 @@
+package com.application.cab_application.Servlets;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "DriverLoginServlet", value = "/DriverLoginServlet")
+public class DriverLoginServlet extends HttpServlet {
+
+    public void init() {
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+    public void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+    public void destroy() {
+    }
+}
---
+++ src/main/java/com/application/cab_application/Servlets/RideLoginServlet.java
@@ -0,0 +1,16 @@
+package com.application.cab_application.Servlets;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+
+public class RideLoginServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+    public void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+}
