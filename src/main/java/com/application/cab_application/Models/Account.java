500b3a6dfe5127e3721e424b93bf572db385e788
bharath <bharathbk2121@gmail.com>
2024-06-26 18:17:53 +0530

    Validation for email and password

--- src/main/java/com/application/cab_application/Models/Account.java
+++ src/main/java/com/application/cab_application/Models/Account.java
@@ -3,6 +3,9 @@ package com.application.cab_application.Models;
 import com.application.cab_application.enums.AccountType;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class Account {
     private int id;
@@ -17,6 +20,43 @@ public class Account {
         this.password = password;
         this.phoneNumber = phoneNumber;
         this.accountType = accountType;
+    }
+
+
+    public List<String> checkValidations(Account account){
+        List<String> errors = new ArrayList<>();
+        if(!checkEmailValidation(account.getEmail())){
+            errors.add("Email is not Valid");
+        }
+        if(!checkPasswordValidation(account.getPassword())){
+            errors.add("Password is not valid");
+        }
+        return errors;
+    }
+
+    public static Boolean checkEmailValidation(String email){
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
+    }
+
+    public Boolean checkPasswordValidation(String password){
+        String regex = "^(?=.*[0-9])"
+                + "(?=.*[a-z])(?=.*[A-Z])"
+                + "(?=.*[@#$%^&+=])"
+                + "(?=\\S+$).{8,20}$";
+
+        Pattern p = Pattern.compile(regex);
+        if (password == null) {
+            return false;
+        }
+        return p.matcher(password).matches();
     }
 
 
