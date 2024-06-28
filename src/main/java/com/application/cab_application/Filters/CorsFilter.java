ceb6db65ce39a27c611b1c2326b03c251f07b71a
bharath <bharathbk2121@gmail.com>
2024-06-28 11:45:10 +0530

    Added Cookie in to the entire path

--- src/main/java/com/application/cab_application/Filters/CorsFilter.java
+++ src/main/java/com/application/cab_application/Filters/CorsFilter.java
@@ -29,6 +29,7 @@ public class CorsFilter extends HttpFilter {
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
--- src/main/java/com/application/cab_application/Servlets/DriverLoginServlet.java
+++ src/main/java/com/application/cab_application/Servlets/DriverLoginServlet.java
@@ -4,15 +4,20 @@ import java.io.*;
 
 import com.application.cab_application.Models.Account;
 import com.application.cab_application.Services.AccountService;
+import com.application.cab_application.Util.ReadJson;
 import com.application.cab_application.enums.AccountType;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import jakarta.servlet.http.*;
 
 
 
 public class DriverLoginServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
+        String requestBody = ReadJson.convertJsonToString(request.getReader());
+        JsonObject jsonObject = new Gson().fromJson(requestBody, JsonObject.class);
+        String email = jsonObject.get("email").getAsString();
+        String password = jsonObject.get("password").getAsString();
         Account loggedAccount = AccountService.authenticateUser(email,password, AccountType.DRIVER);
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
@@ -21,7 +26,9 @@ public class DriverLoginServlet extends HttpServlet {
             session.setAttribute("userID",loggedAccount.getId());
             session.setAttribute("accountType", "DRIVER");
             session.setMaxInactiveInterval(7 * 60 * 60);
-            response.addCookie(new Cookie("JSESSIONID", session.getId()));
+            Cookie sessionCookie = new Cookie("JSESSIONID", session.getId());
+            sessionCookie.setPath("/");
+            response.addCookie(sessionCookie);
             response.setStatus(HttpServletResponse.SC_OK);
         }else{
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
