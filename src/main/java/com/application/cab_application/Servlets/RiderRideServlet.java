9cd429d77ba8b57fdb52484f06c0bb28f7a3ca97
bharath <bharathbk2121@gmail.com>
2024-06-29 17:41:49 +0530

    Rider ride validations added

--- src/main/java/com/application/cab_application/Servlets/RiderRideServlet.java
+++ src/main/java/com/application/cab_application/Servlets/RiderRideServlet.java
@@ -10,6 +10,7 @@ import com.application.cab_application.Models.Account;
 import com.application.cab_application.Models.AccountDetails;
 import com.application.cab_application.Models.Ride;
 import com.application.cab_application.Models.RideDetails;
+import com.application.cab_application.Util.CurrentUserHelper;
 import com.application.cab_application.enums.RequestStatus;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
@@ -24,10 +25,10 @@ public class RiderRideServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Gson gson = new Gson();
         response.setContentType("application/json");
-        String accountId = request.getParameter("accountID");
+        int accountId = CurrentUserHelper.getAccount();
         response.setCharacterEncoding("UTF-8");
         PrintWriter writer = response.getWriter();
-        List<JsonObject> objectList = RidesDao.getAllRideDetails(Integer.parseInt(accountId),"RIDER");
+        List<JsonObject> objectList = RidesDao.getAllRideDetails(accountId,"RIDER");
         String responseValues = gson.toJson(objectList);
         response.setStatus(HttpServletResponse.SC_OK);
         writer.write(responseValues);
@@ -35,6 +36,7 @@ public class RiderRideServlet extends HttpServlet {
 
     public void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String rideID = request.getParameter("rideID");
+        int accountID = CurrentUserHelper.getAccount();
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         PrintWriter printWriter = response.getWriter();
@@ -42,6 +44,12 @@ public class RiderRideServlet extends HttpServlet {
         if(ride.getId() == 0){
             response.setStatus(422);
             printWriter.write("{\"message\":\"Enter Valid RideID\"}");
+            return;
+        }
+        RideDetails rideDetails = RideDetailsDao.getRideDetails(ride.getId());
+        if(ride.getRiderId()!=accountID || rideDetails.getRequestStatus() == RequestStatus.CANCELLED || rideDetails.getRequestStatus()== RequestStatus.ENDED){
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            printWriter.write("{\"message\":\"There was some error\"}");
             return;
         }
         AccountDetailsDao.updateCurrentRideIDAsNUll(ride.getDriverId());
